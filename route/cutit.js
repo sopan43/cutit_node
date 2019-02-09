1f8ca1bb99e8851a91a9e3bc42c67dc5210894d5
Sopan <sopan.mittal@weaveability.com>
2019-02-10 04:14:37 +0530

    modify generate unique key and post route

--- route/cutit.js
+++ route/cutit.js
@@ -3,7 +3,7 @@ const express = require('express');
 const randomstring = require('randomstring');
 const bodyParser = require('body-parser');
 const path = require('path');
-const sgMail = require('@sendgrid/mail');
+// const sgMail = require('@sendgrid/mail');
 const router = express.Router();
 const app = express();
 
@@ -26,43 +26,7 @@ app.post('/short', (req, res) => {
         console.log(req.body.custom_url);
         res.redirect('/');
     } else if (req.body.custom_url !== '') {
-        conn.query('SELECT * FROM short_urls WHERE short_code = ?', [req.body.custom_url], (error, data) => {
-            if (error) {
-                return res.json({ success: 0, message: 'error' });
-            } else if (data.length === 0) {
-                var upload_object = {
-                    long_url: req.body.original_url,
-                    short_code: req.body.custom_url
-                };
-                url = req.headers.host + '/' + upload_object.short_code;
-                conn.query('INSERT INTO short_urls SET ?', [upload_object], (error, response) => {
-                    if (error) {
-                        return res.json({ success: 0, message: 'error' });
-                    } else {
-                        res.redirect('/short/url');
-                    }
-                });
-            } else {
-                console.log(data);
-                genrate_unique_key().then((rstring) => {
-                    url = req.headers.host + '/' + rstring;
-                    var upload_object = {
-                        long_url: req.body.original_url,
-                        short_code: rstring
-                    };
-                    conn.query('INSERT INTO short_urls SET ?', [upload_object], (error, response) => {
-                        if (error) {
-                            return res.json({ success: 0, message: 'error' });
-                        } else {
-                            res.redirect('/short/url');
-                        }
-                    });
-
-                }, (error) => {
-                    return res.json({ success: 0, message: 'Error ' + error });
-                });
-            }
-        });
+        
     } else {
         genrate_unique_key().then((rstring) => {
             url = req.headers.host + '/' + rstring;
@@ -70,15 +34,13 @@ app.post('/short', (req, res) => {
                 long_url: req.body.original_url,
                 short_code: rstring
             };
-            conn.query('INSERT INTO short_urls SET ?', [upload_object], (error, response) => {
-                if (error) {
-                    return res.json({ success: 0, message: 'error' });
-                } else {
-                    res.redirect('/short/url');
-                }
+            conn.execute('INSERT INTO short_urls (long_url, short_code) VALUES (?, ?)', [req.body.original_url, rstring])
+            .then(() => {
+                res.redirect('/short/url');
+            }).catch((error) => {
+                return res.json({ success: 0, message: 'error' });
             });
-
-        }, (error) => {
+        }).catch((error) => {
             return res.json({ success: 0, message: 'Error ' + error });
         });
     }
@@ -93,37 +55,37 @@ app.get('/short/url', (req, res) => {
 });
 
 
-app.post('/feedback', (req, res) => {
-    var text = req.body.feedback;
-    var name = req.body.name;
-    var email = req.body.email;
-    if (name === '') {
-        name = 'No name'
-    }
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-        to: 'developer.cutit@gmail.com',
-        from: 'sopanmittal43@gmail.com',
-        subject: 'Feedback by ' + name,
-        text: text
-    };
-    sgMail.send(msg);
+// app.post('/feedback', (req, res) => {
+//     var text = req.body.feedback;
+//     var name = req.body.name;
+//     var email = req.body.email;
+//     if (name === '') {
+//         name = 'No name'
+//     }
+//     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+//     const msg = {
+//         to: 'developer.cutit@gmail.com',
+//         from: 'sopanmittal43@gmail.com',
+//         subject: 'Feedback by ' + name,
+//         text: text
+//     };
+//     sgMail.send(msg);
 
-    
-    if (email !== '') {
-            sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        console.log(email);
-        const msg2 = {
-            to: email,
-            from: 'info.cutit@gmail.com',
-            subject: 'Thanks',
-            text: 'text'
-        };
-        sgMail.send(msg2);
 
-    }
-    res.send('DONE');
-});
+//     if (email !== '') {
+//         sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+//         console.log(email);
+//         const msg2 = {
+//             to: email,
+//             from: 'info.cutit@gmail.com',
+//             subject: 'Thanks',
+//             text: 'text'
+//         };
+//         sgMail.send(msg2);
+
+//     }
+//     res.send('DONE');
+// });
 
 
 app.get('s/:cut', (req, res) => {
@@ -146,9 +108,9 @@ app.get('s/:cut', (req, res) => {
 function genrate_unique_key() {
     return new Promise((resolve, reject) => {
         var rstring = randomstring.generate(7);
-        check_unique_key(rstring).then((value) => {
+        check_unique_key(rstring).then(value => {
             resolve(value);
-        }, (error) => {
+        }).catch(error => {
             reject(error);
         });
     });
@@ -156,20 +118,21 @@ function genrate_unique_key() {
 
 function check_unique_key(rstring, callback) {
     return new Promise((resolve, reject) => {
-        conn.query('SELECT short_code FROM short_urls WHERE short_code = ?', [rstring], (error, result) => {
-            if (error) {
-                reject('error in query ' + error);
-            } else if (result.length === 0) {
+        conn.execute('SELECT short_code FROM short_urls WHERE short_code = ?', [rstring]).
+        then(result => {
+            console.log(result);
+            if (result[0].length === 0) {
                 resolve(rstring);
             } else {
-                console.log('===========');
+                // console.log('===========');
                 genrate_unique_key();
             }
+
+        }).catch(error => {
+            reject('error in query ' + error);
         });
     });
 }
-
-app.post('/feedback')
 
 
 module.exports = app;
